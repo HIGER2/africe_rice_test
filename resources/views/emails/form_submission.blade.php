7ade650066cf188339ea9fccbd6adc2d8ffd0aec
doumaarmand@gmail.com <doumaarmand@gmail.com>
2024-08-17 23:35:55 +0000

    modif

--- resources/views/emails/form_submission.blade.php
+++ resources/views/emails/form_submission.blade.php
@@ -6,14 +6,14 @@ Bonjour,
 L'utilisateur **{{ $user->firstName." ".$user->lastName }}**
  a soumis un nouveau demande de départ pour le **{{$form->depart_date}}**.
 
-{{--
+
 @component('mail::button', ['url' => route('form.action', ['id' => $form->id, 'action' => 'approve']), 'color' => 'green'])
 Approuver
 @endcomponent
 
 @component('mail::button', ['url' => route('form.action', ['id' => $form->id, 'action' => 'reject']), 'color' => 'red'])
 Rejeter
-@endcomponent --}}
+@endcomponent
 {{--
 Merci de vérifier.
 
