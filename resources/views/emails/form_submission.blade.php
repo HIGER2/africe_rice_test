898821639d21e9758141811be1ab5a137358efcf
doumaarmand@gmail.com <doumaarmand@gmail.com>
2024-10-17 20:14:07 +0000

    modif

--- resources/views/emails/form_submission.blade.php
+++ resources/views/emails/form_submission.blade.php
@@ -7,14 +7,6 @@ The staff **{{ $user->firstName." ".$user->lastName }}** has submitted the depar
 The departure request is for **{{$form->depart_date}}**.
 The taking up of office is scheduled for **{{$form->taking_date}}**.
 
-<x-mail::button :url="URL::signedRoute('form.confirm', ['id' => $form->id, 'action' => 'approve'])" color="success">
-Approve
-</x-mail::button>
-
-<x-mail::button :url="URL::signedRoute('form.confirm', ['id' => $form->id, 'action' => 'reject'])" color="error">
-Reject
-</x-mail::button>
-
 {{ config('app.name') }}
 
 </x-mail::message>
