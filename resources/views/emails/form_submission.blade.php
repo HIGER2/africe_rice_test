9be98c0db910d239825dba50e3aac049058f9814
doumaarmand@gmail.com <doumaarmand@gmail.com>
2024-10-17 15:56:06 +0000

    test

--- resources/views/emails/form_submission.blade.php
+++ resources/views/emails/form_submission.blade.php
@@ -2,7 +2,7 @@
 # New request submitted N° {{$form->request_number}}
 
 Hello,
-
+{{--
 The staff **{{ $user->firstName." ".$user->lastName }}** has submitted the departure form for Bouaké.
 The departure request is for **{{$form->depart_date}}**.
 The taking up of office is scheduled for **{{$form->taking_date}}**.
@@ -13,13 +13,10 @@ Approve
 
 @component('mail::button', ['url' => URL::signedRoute('form.confirm', ['id' => $form->id, 'action' => 'reject']), 'color' => 'red'])
 Reject
-@endcomponent
-{{ config('app.name') }}
+@endcomponent --}}
 
-{{--
 Thank you for reviewing this.
 
-Best regards,<br>
 
-{{ config('app.name') }} --}}
+{{-- {{ config('app.name') }}  --}}
 @endcomponent
